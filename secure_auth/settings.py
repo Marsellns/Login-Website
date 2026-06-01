96d2cea4a15b46f264b7acbae45ee7d5a4db30f6
Marsellns <marsellsmjr@gmail.com>
2026-06-01 15:18:32 +0700

    Add django-sslserver to requirements

--- secure_auth/settings.py
+++ secure_auth/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
     # Project apps
     "accounts",                      # Our custom authentication app
+    "sslserver",
 ]
 
 # ──────────────────────────────────────────────────────────────────────────────
